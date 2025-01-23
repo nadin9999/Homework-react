69566e7d10cce6661abc37a80f75a48281d54999
nadin9999 <nadinanarbekova8@gmail.com>
2025-01-23 21:07:14 +0600

    Добавлены все файлы проекта

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import FourMain from "./FourMain";
+import FourMainCard from "./FourMainCard";
 import HeaderComponent from "./Header";
 import MainComponent from "./Main"
 import SecondMain from "./SecondMain";
@@ -8,6 +10,11 @@ import Image1 from "./image/Image1.png"
 import Image2 from "./image/Image2.png"
 import Image3 from "./image/Image3.png"
 import Image4 from "./image/Image4.png"
+import Image5 from "./image/Image5.png"
+import Image6 from "./image/Image6.png"
+import Image7 from "./image/Image7.png"
+import Image8 from "./image/Image8.png"
+
 
 function App() {
   return (
@@ -21,6 +28,13 @@ function App() {
     <ThirdMainCard image = {Image2} name = "GREEN MUSCLE FIT POLO SHIRT" price = "$120"/>
     <ThirdMainCard image = {Image3} name = "GREEN MUSCLE FIT POLO SHIRT" price = "$128"/>
     <ThirdMainCard image = {Image4} name = "GREEN MUSCLE FIT POLO SHIRT" price = "$122"/>
+    </div>
+    <FourMain/>
+    <div className="Card" style={{display: "flex", gap: '22px', padding:'111px 228px 111px'}}>
+    <FourMainCard image = {Image5} name = "GREEN MUSCLE FIT POLO SHIRT" price = "$129"/>
+    <FourMainCard image = {Image6} name = "GREEN MUSCLE FIT POLO SHIRT" price = "$129"/>
+    <FourMainCard image = {Image7} name = "GREEN MUSCLE FIT POLO SHIRT" price = "$129"/>
+    <FourMainCard image = {Image8} name = "GREEN MUSCLE FIT POLO SHIRT" price = "$129"/>
     </div>
     </div>
   );
---
+++ src/FourMain.jsx
@@ -0,0 +1,14 @@
+import "./FourMain.css"
+function FourMain(){
+    return(
+        <div className="FourdMain">
+
+            <div className="firstSection">
+            <p id="P1">MEN’S FASHION</p>
+            <p id="P2">Shop our new arrivals from established brands</p>
+            </div>
+
+        </div>
+    )
+}
+export default FourMain;
